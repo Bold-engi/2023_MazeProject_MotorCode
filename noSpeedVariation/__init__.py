9e56cd5a0e409f211f2539dd9896b64c5d043b05
Yumo <60574298+Bold-engi@users.noreply.github.com>
2023-02-08 21:41:56 +0000

    Update __init__.py

--- noSpeedVariation/__init__.py
+++ noSpeedVariation/__init__.py
@@ -5,8 +5,7 @@ class opProfile:
   def __init__(self):
     
     # Self-explained
-    self._direction       = CW
-    self._cur_steps       = 0
+    self._steps_done      = 0
     self._target_steps    = 0
     self._rotor_radius_mm = 4.5/2
     
@@ -19,10 +18,3 @@ class opProfile:
 
   def set_target_steps(self):
     self._target_steps = self.calc_steps()
-    
-  def current_steps(self):
-    
-    if self._direction == CW:
-      self._cur_steps += 1
-    elif self._direction == CCW:
-      self._cur_steps -= 1
