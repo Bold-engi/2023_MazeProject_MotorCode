70661e7a29ed7a99a7787b417ceda8425b40c742
Yumo <60574298+Bold-engi@users.noreply.github.com>
2023-02-25 12:47:43 +0000

    Update operation.py

--- noSpeedVariation/operation.py
+++ noSpeedVariation/operation.py
@@ -1,24 +1,25 @@
 import numpy
 
 class opProfile:
-  
-  def __init__(self):
     
-    # Self-explained
-    self._cur_step        = 0
-    self._steps_done      = 0
-    self._target_steps    = 0
-    self._rotor_radius_mm = 4.5/2
+    def __init__(self):
+        
+        self._cur_step      = -1
+        self._steps_done    = 0
+        self._tar_steps     = 0
+        self._rt_radius_mm  = 4.5/2
+        
+    def set_distance(self, distance_mm):
+        self._distance_mm = distance_mm
+        print("DISTANCE(mm) = ", self._distance_mm)
+        
+    def calc_steps(self):
+        return round((self._distance_mm/self._rt_radius_mm)*(180/numpy.pi))
     
-  def set_distance(self, distance_mm):
-    self._distance_mm = distance_mm
-  
-  def calc_steps(self):
-    # calculate steps required to achieve the target displacement
-    return round((self._distance_mm/self._rotor_radius_mm)*(180/numpy.pi))
-
-  def set_target_steps(self):
-    self._target_steps = self.calc_steps()
-
-  def current_step(self):
-    self._cur_step += 1
+    def set_target_steps(self):
+        self._tar_steps = self.calc_steps()
+        return self._tar_steps
+        
+    def current_step(self):
+        self._cur_step += 1
+        return self._cur_step
