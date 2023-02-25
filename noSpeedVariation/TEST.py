c9ffe4bdd52741ec9b3a2f961ba71ec2e2886862
Yumo <60574298+Bold-engi@users.noreply.github.com>
2023-02-25 12:48:30 +0000

    Update TEST.py

--- noSpeedVariation/TEST.py
+++ noSpeedVariation/TEST.py
@@ -3,18 +3,19 @@ from setup import DRV8825
 import RPi.GPIO as GPIO
 import numpy, time
 
-DIR = 16
-STEP = 12
-EN = 18
+DIR = 20
+STEP = 21
+EN = 16
 
 op = opProfile()
 drv = DRV8825(DIR, STEP, EN)
 
+drv.enable()
 drv.on()
-op.set_distance(100)
+op.set_distance(5)
 t_steps = op.set_target_steps()
 c_step  = op.current_step()
 
 while c_step <= t_steps:
-  drv.oneStep(1)
-  time.sleep(.005)
+    drv.oneStep(0)
+    time.sleep(.01)
