ec2f6f6af1c5d7877541c779ee13f3f1b86792ac
Yumo <60574298+Bold-engi@users.noreply.github.com>
2023-02-25 12:46:40 +0000

    Update setup.py

--- noSpeedVariation/setup.py
+++ noSpeedVariation/setup.py
@@ -2,40 +2,38 @@ import RPi.GPIO as GPIO
 import time
 
 class DRV8825:
-  
-  def __init__(self, dir_pin, step_pin, en_pin):
     
-    self._dir_pin = dir_pin
-    self._step_pin = step_pin
-    self._en_pin = en_pin
-    
-  def enable(self):
-    if self._en_pin:
-      GPIO.setup(self._en_pin, GPIO.OUT, initial=GPIO.LOW)
-    print('Driver enabled')
-      
-  def disable(self):
-    if self._en_pin:
-      GPIO.setup(self._en_pin, GPIO.OUT, initial=GPIO.HIGH)
-    print('Driver disabled')
-      
-  def on(self):
-    
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    
-    GPIO.setup(self._dir_pin, GPIO.OUT, inital=GPIO.HIGH)
-    GPIO.setup(self._step_pin, GPIO.OUT, inital=GPIO.LOW)
-    
-    enable()
-    
-  def off(self):
-    disable()
-    
-  def oneStep(self, direction):
-    
-    GPIO.output(self._dir_pin, GPIO.HIGH if direction==1 else GPIO.LOW)
-    
-    GPIO.output(self._step_pin, GPIO.HIGH)
-    time.sleep(.005)
-    GPIO.output(self._step_pin, GPIO.LOW)
+    def __init__(self, dir_pin, step_pin, en_pin):
+        
+        self._dir_pin  = dir_pin
+        self._step_pin = step_pin
+        self._en_pin   = en_pin
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        
+    def enable(self):
+        if self._en_pin:
+            GPIO.setup(self._en_pin, GPIO.OUT, initial=GPIO.LOW)
+        print("DRIVER ENABLED")
+        
+    def disable(self):
+        if self._en_pin:
+            GPIO.setup(self._en_pin, GPIO.OUT, initial=GPIO.HIGH)
+        print("DRIVER DISABLED")
+        
+    def on(self):
+        
+        GPIO.setup(self._dir_pin, GPIO.OUT, initial=GPIO.HIGH)
+        GPIO.setup(self._step_pin, GPIO.OUT, initial=GPIO.LOW)
+        
+    def oneStep(self, direction, stepdelay):
+        
+        GPIO.output(self._dir_pin, GPIO.HIGH if direction==1 else GPIO.LOW)
+        print("DIR pin is: ", self._dir_pin)
+        
+        GPIO.output(self._step_pin, GPIO.LOW)
+        print("STEP: ", self._step_pin)
+        time.sleep(stepdelay)
+        GPIO.output(self._step_pin, GPIO.HIGH)
+        print("STEP: ", self._step_pin)
