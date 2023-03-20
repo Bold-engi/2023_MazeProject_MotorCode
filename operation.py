b7b777af2fd527cd1379d12a89b66a6e111ac366
Yumo <60574298+Bold-engi@users.noreply.github.com>
2023-03-20 23:38:53 +0000

    Update operation.py

--- operation.py
+++ operation.py
@@ -1,3 +1,4 @@
+'''
 import math
 
 class opProfile:
@@ -16,16 +17,24 @@ class opProfile:
 
     def calc_steps(self):
         '''
+        '''
         Returns steps requiered to move a certain displacement
         '''
+        '''
         return round(self._distance/(self._rt_radius_mm * self._step_angle))
-    
+        '''
+        '''
     def diag_to_ax(self):
+        '''
         '''
         Returns axial displacement
         
         POSITIVE regardless x-axis is +ve or -ve
         Less than 90 degrees
         '''
+        '''
         return [self._distance_mm * math.cos(math.radians(self._angle_deg)),
                 self._distance_mm * math.sin(math.radians(self._angle_deg))]
+        '''
+
+  # This file is no longer needed as no motors aren't running with a pre-setting distance
