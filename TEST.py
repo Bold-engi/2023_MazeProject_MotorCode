d6228b6a9bab51df4a5b2516ff34efcc9c0d9d3c
Yumo <60574298+Bold-engi@users.noreply.github.com>
2023-03-22 14:30:33 +0000

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,6 +1,6 @@
 from operation import opProfile as OP
 from run import DRV8825 as DRV
-import asyncio, time, math
+import time, math, client, socket
 
 X_DIR = 20
 X_STEP = 16
@@ -10,18 +10,20 @@ Y_DIR = 19
 Y_STEP = 13
 Y_EN = 26
 
+client.connect()
+
+data = client.receive()
+
+direction = 1
+axis = 'X'
+
 R = DRV(X_DIR, X_STEP, X_EN, Y_DIR, Y_STEP, Y_EN)
 
 R.enable()
 
 R.on()
 
-time.sleep(2)
-
-R.move_axial('X', 50, 1, .005)
-
-time.sleep(2)
-
-R.move_axial('Y', 30 ,0, .005)
-
-asyncio.run(R.move_diag(50,30,1,1,5))
+while True:
+  '''
+  code to run the motor based on received data
+  '''
